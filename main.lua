92a7c0c87591e0a45ad847d2d86c8d669eb10958
Ix1x0x3 <96854760+Ix1x0x3@users.noreply.github.com>
2022-01-10 19:56:08 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
-local RespectedFile = game:HttpGet("");
+function Notify(txt)
+  game.StarterGui:SetCore("SendNotification", {
+			Title = "Ix Hub",
+			Text = Text
+	})
+end
+
+Notify("Thanks for using ixHub! We are a free hub, so if you bought this, please tell us from who! This is free!");
+
+local RespectedFile = game:HttpGet("https://raw.githubusercontent.com/Ix1x0x3/ixHub/main/games/"..game.PlaceId..".ix.lua");
+
+if RespectedFile == "404: Not Found" then
+  return Notify("Your game isn't in ixHub!");
+end
+
+Notify("Script loaded for "..game.Name..".")
+loadstring(RespectedFile, true);
